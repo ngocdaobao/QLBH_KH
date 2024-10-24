f35372c90a5ac8009e2c02ea0ba8a38a9548e4ee
kimcuongdev <kimcuong09102004@gmail.com>
2024-10-24 09:21:29 +0700

    basic view

--- src/main/java/com/project/qlbh_kh/MainApp.java
+++ src/main/java/com/project/qlbh_kh/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("views/MainAppView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(),1280,640);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
