02ef72e816911ffb7e544197d51a0f1919c35722
kimcuongdev <kimcuong09102004@gmail.com>
2024-10-27 01:24:53 +0700

    kiem kho basic view

--- src/main/java/com/project/qlbh_kh/controllers/kiemKhoController.java
+++ src/main/java/com/project/qlbh_kh/controllers/kiemKhoController.java
@@ -1,4 +1,53 @@
 package com.project.qlbh_kh.controllers;
 
+import com.project.qlbh_kh.entity.matHang;
+import com.project.qlbh_kh.utils.JDBCUtil;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 public class kiemKhoController {
+    @FXML
+    private TableView<matHang> tableView;
+    @FXML
+    private TableColumn<matHang, String> prodNameColumn;
+    @FXML
+    private TableColumn<matHang,String> prodQuantityColumn;
+    @FXML
+    public void initialize() {
+        prodNameColumn.setCellValueFactory(new PropertyValueFactory<>("tenMatHang"));
+        prodQuantityColumn.setCellValueFactory(new PropertyValueFactory<>("soLuong"));
+
+        loadStockData();
+    }
+    private void loadStockData() {
+        ObservableList<matHang> matHangList = FXCollections.observableArrayList();
+        Connection conn = null;
+
+        try {
+            conn = JDBCUtil.getConnection();
+            String query = "SELECT prod_name, quantity FROM stock_tb";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String tenMatHang = rs.getString(1);
+                int soLuong = rs.getInt(2);
+                matHangList.add(new matHang(tenMatHang, soLuong));
+            }
+            tableView.setItems(matHangList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.closeConnection(conn);
+        }
+    }
 }
--- src/main/java/com/project/qlbh_kh/entity/matHang.java
+++ src/main/java/com/project/qlbh_kh/entity/matHang.java
@@ -1,4 +1,22 @@
 package com.project.qlbh_kh.entity;
 
 public class matHang {
+    private String tenMatHang;
+    private int soLuong;
+    private String donViTinh;
+    private double giaNhapVao;
+    private double giaBanRa;
+
+    public matHang(String tenMatHang, int soLuong) {
+        this.tenMatHang = tenMatHang;
+        this.soLuong = soLuong;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public String getTenMatHang() {
+        return tenMatHang;
+    }
 }
