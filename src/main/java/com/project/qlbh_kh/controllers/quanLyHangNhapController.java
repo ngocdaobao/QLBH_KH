f0dd73c9ef4f1f61d99ce99b6ab3ba438c9bf7f2
kimcuongdev <kimcuong09102004@gmail.com>
2024-10-27 09:07:12 +0700

    commit

--- src/main/java/com/project/qlbh_kh/controllers/quanLyHangNhapController.java
+++ src/main/java/com/project/qlbh_kh/controllers/quanLyHangNhapController.java
@@ -1,4 +1,40 @@
 package com.project.qlbh_kh.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+
 public class quanLyHangNhapController {
+
+    @FXML
+    private TableColumn<?, ?> customer_in_id;
+
+    @FXML
+    private Button execute;
+
+    @FXML
+    private DatePicker fromDate;
+
+    @FXML
+    private TableColumn<?, ?> order_date;
+
+    @FXML
+    private TableColumn<?, ?> order_in_id;
+
+    @FXML
+    private TableColumn<?, ?> prod_id;
+
+    @FXML
+    private TableColumn<?, ?> quantity;
+
+    @FXML
+    private DatePicker toDate;
+
+    @FXML
+    private TableColumn<?, ?> total_amount;
+
+    @FXML
+    private TableColumn<?, ?> unit_price;
+
 }
--- src/main/java/com/project/qlbh_kh/controllers/truyXuatKhoController.java
+++ src/main/java/com/project/qlbh_kh/controllers/truyXuatKhoController.java
@@ -1,4 +1,22 @@
 package com.project.qlbh_kh.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
 public class truyXuatKhoController {
+
+    @FXML
+    private Button execute;
+
+    @FXML
+    private DatePicker fromDate;
+
+    @FXML
+    private ComboBox<?> operation;
+
+    @FXML
+    private DatePicker toDate;
+
 }
